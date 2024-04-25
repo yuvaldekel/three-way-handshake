97586dd7f9fd197d9e3fa77e1ede741882682d1b
Yuval Dekel <yuvddd05@gmail.com>
2024-04-25 13:03:55 +0300

    add sport

--- create_syn.py
+++ create_syn.py
@@ -1,14 +1,14 @@
 from scapy.all import TCP, IP, send, sr1
 
 def main():
-    syn_segment = IP(dst = 'www.google.com')/TCP(dport = 80, seq = 123, flags = 2)
+    syn_segment = IP(dst = 'www.google.com')/TCP(sport = 10000, dport = 80, seq = 123, flags = 2)
     syn_segment.show()
     
     syn_ack_packet = sr1(syn_segment)
     syn_ack_packet.show()
     next = syn_ack_packet[TCP].seq + 1
     
-    ack_segment = IP(dst = 'www.google.com')/TCP(dport = 80, seq = syn_ack_packet[TCP].ack, ack = next, flags = 16)
+    ack_segment = IP(dst = 'www.google.com')/TCP(sport = 10000, dport = 80, seq = syn_ack_packet[TCP].ack, ack = next, flags = 16)
     ack_segment.show()
     send(ack_segment)
          
